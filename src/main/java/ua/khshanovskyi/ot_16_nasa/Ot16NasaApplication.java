39d56df944756e788ee7005470f37c53c9ffccb3
Pavlo Khshanovskyi <pkhshan@emory.edu>
2022-07-11 19:09:08 +0300

    HW 17

--- src/main/java/ua/khshanovskyi/ot_16_nasa/Ot16NasaApplication.java
+++ src/main/java/ua/khshanovskyi/ot_16_nasa/Ot16NasaApplication.java
@@ -1,14 +1,30 @@
 package ua.khshanovskyi.ot_16_nasa;
 
+import lombok.extern.log4j.Log4j2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import ua.khshanovskyi.ot_16_nasa.service.PictureService;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+
+import java.util.concurrent.TimeUnit;
 
 @SpringBootApplication
+@EnableCaching
+@EnableScheduling
+@Log4j2
 public class Ot16NasaApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(Ot16NasaApplication.class, args);
+    }
+
+
+    @Scheduled(timeUnit = TimeUnit.HOURS, fixedRate = 24)
+    @CacheEvict(value = "maxImageUrl")
+    public void invalidateCashes() {
+        log.info("Caches are invalidated");
     }
 
 }
--- src/main/java/ua/khshanovskyi/ot_16_nasa/controller/PictureController.java
+++ src/main/java/ua/khshanovskyi/ot_16_nasa/controller/PictureController.java
@@ -12,6 +12,7 @@ import ua.khshanovskyi.ot_16_nasa.exception.NoUriException;
 import ua.khshanovskyi.ot_16_nasa.service.PictureService;
 import ua.khshanovskyi.ot_16_nasa.service.UriConfigurationService;
 
+import javax.servlet.http.HttpServletRequest;
 import java.net.URI;
 import java.util.LinkedHashMap;
 import java.util.Map;
@@ -24,12 +25,14 @@ public class PictureController {
     private static final String HOST = "https://api.nasa.gov";
     private static final String PATH = "/mars-photos/api/v1/rovers/curiosity/photos";
     private static final String KEY = "A8TWOxIOLYey639GppGaUsthUE3etmlDlYr1MfbS";
+    private final HttpServletRequest request;
 
     private PictureService pictureService;
     private UriConfigurationService uriConfigurationService;
 
     @GetMapping("/{sol}/largest")
     public ResponseEntity<URI> getMaxPictureBySol(@PathVariable int sol){
+        log.info(request.getRemoteAddr() + " is trying to get max picture by sol = " + sol);
         URI uriWithMaxPictureSize;
         try {
             uriWithMaxPictureSize = pictureService.getUriWithMaxPictureSize(uriConfigurationService.generateUri(HOST, PATH, getParametersMap(sol)));
